37359675b444e5443d6924f6c24eb9698270f0cd
Kiran Deep <kirandeep102030@gmail.com>
2022-11-03 22:49:46 +0530

    Added waitpid()

--- Week-67/3.2.c
+++ Week-67/3.2.c
@@ -1,11 +1,35 @@
-// Aim : Waits for a child process to stop or end
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-// wait() starts
-#include<sys/wait.h>
-// wait() ends
+int main (){
+    pid_t pid;
+    int status;
 
-// pid_t, fork() Usage start
-#include<unistd.h>
-#include<sys/types.h>
-// pid_t, fork() Usage ends
+    // printf("Parent: %d\n", getpid());
+
+    pid = fork();
+    if (pid == 0){
+        printf("Child %d\n", getpid());
+        sleep(2);
+        exit(EXIT_SUCCESS);
+    }
+
+    //Comment from here to...
+    //Parent waits process pid (child)
+    waitpid(pid, &status, 0);
+    //Option is 0 since I check it later
+
+    if (WIFSIGNALED(status)){
+        printf("Error\n%d", status);
+    }
+    else if (WIFEXITED(status)){
+        printf("Exited Normally\n");
+    }
+    //To Here and see the difference
+    printf("Parent: %d\n", getpid());
+
+    return 0;
+}
