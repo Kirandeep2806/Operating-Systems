9a83a9bcb5828a381ca9f66976c90bdc9054fd1d
Kiran Deep <kirandeep102030@gmail.com>
2022-11-04 01:11:20 +0530

    Added other code samplesgit add -A

--- Week-4/5.c
+++ Week-4/5.c
@@ -6,3 +6,33 @@ int main() {
     printf("Ending of 2.c!!");
     return 0;
 }
+
+/*
+
+#include <unistd.h>
+
+int ret;
+char *cmd[] = { "ls", "-l", (char *)0 };
+...
+ret = execv ("/bin/ls", cmd);
+
+===============================================
+
+#include <unistd.h>
+
+int ret;
+char *cmd[] = { "ls", "-l", (char *)0 };
+char *env[] = { "HOME=/usr/home", "LOGNAME=home", (char *)0 };
+...
+ret = execve ("/bin/ls", cmd, env);
+
+================================================
+
+#include <unistd.h>
+
+int ret;
+char *cmd[] = { "ls", "-l", (char *)0 };
+...
+ret = execvp ("ls", cmd);
+
+*/
