268f8fb432dfdaf9783ad0f329d8a87c6447d252
Kiran Deep <kirandeep102030@gmail.com>
2022-11-04 01:26:19 +0530

    Added orphan process code

---
+++ Week-4/6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main() {
+    pid_t pid=fork();
+    if(pid<0)
+        perror("Fork Error!!\n");
+    else if(pid>0) {
+        printf("Parent Process\n");
+    }
+    else {
+        sleep(2);
+        printf("Child Process\n");
+    }
+    return 0;
+}
