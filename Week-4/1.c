a021797de44528e5c9dac0ac9cc8107c5a714c50
Kiran Deep <kirandeep102030@gmail.com>
2022-12-08 23:47:34 +0530

    Minor changes

--- Week-4/1.c
+++ Week-4/1.c
@@ -1,9 +1,6 @@
-// gcc 1.c -o 1, then goto 2.c and execute it!!
-
 #include<stdio.h>
-#include<unistd.h>
 
 int main() {
-    printf("Hi I am 1.c File\n");
-    return 0;
+	printf("Namasthe!!");
+	return 0;
 }
