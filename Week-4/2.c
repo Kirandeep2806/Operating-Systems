3dd30407bc6e3e7699b14fac753659186f4f6fb9
Kiran Deep <kirandeep102030@gmail.com>
2022-12-09 00:14:13 +0530

    Minor changes

--- Week-4/2.c
+++ Week-4/2.c
@@ -2,8 +2,7 @@
 #include<unistd.h>
 
 int main() {
-    char *args[] = {"./1", NULL};
+    char *args[] = {"./main", NULL};
     execv(args[0], args); // execv
-    printf("Ending of 2.c!!");
     return 0;
 }
